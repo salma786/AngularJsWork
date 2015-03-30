8dbab0cabc8c45a1c768bc7407d2a06ac2c14bea
prudhvireddy123 <bonthuprudhvireddy@gmail.com>
2015-03-30 17:14:28 +0530

    learning

---
+++ MyFacebook/myfaecbook/www/js/app.js
@@ -0,0 +1,14 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/')
+    $stateProvider.state('login', {
+        url: '/',
+        templateUrl: 'templates/login.html'
+    })
+})
